6ddb3ea33b1126578ee6b2d18a58d332e9e494a3
Jyri Turku <jyri.turku@student.saimia.fi>
2019-09-12 08:46:01 +0300

    Assignment 1 done, not commented.

--- Repetition/Repetition/Program.cs
+++ Repetition/Repetition/Program.cs
@@ -6,7 +6,58 @@ namespace Repetition
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number;
+            int selection;
+
+            Console.WriteLine("Select the assignment you wish to check,");
+            Console.WriteLine("1 checks assignment 1");
+            Console.WriteLine("2 checks assignments 2 and 4");
+            Console.WriteLine("3 checks assignments 3 and 5");
+            selection = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (selection == 1)
+            {
+                Console.WriteLine("Input number:");
+                number = int.Parse(Console.ReadLine());
+
+                Multiply(number);
+
+            } else if (selection == 2)
+            {
+                Console.WriteLine("This section is being worked on, sorry...");
+            } else if (selection == 3)
+            {
+                Console.WriteLine("This section is being worked on, sorry...");
+            } else
+            {
+                Console.WriteLine("Invalid selection, please reboot.");
+            }
+            
+        }
+
+        static void Multiply(int given)
+        {
+            int number = given;
+            int calc = 1;
+
+            if (number == 0)
+            {
+                Console.WriteLine("Your answer is 1.");
+            } else if (number < 0)
+            {
+                Console.WriteLine("Your answer is undefined");
+            } else
+            {
+                do
+                {
+                    calc *= number;
+                    number -= 1;
+
+                } while (number > 0);
+
+                Console.WriteLine("Your answer is " + calc + ".");
+            }
         }
     }
 }
